6a618652472a8e648436a26440a73efa5426bd67
Marius <marius1890@yahoo.com>
2023-12-31 01:25:26 +0200

    up

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V118.Network;
 using System;
+using System.Net.NetworkInformation;
+using System.Threading;
 
 
 
@@ -19,46 +22,80 @@ namespace UnitTestProject1
         readonly string NEXXON_TARGET = "https://www.nexxon.ro/";
         readonly string BRAND_NAME = "Hankook";
         readonly string USER_SIGEMO = Environment.GetEnvironmentVariable("user_sigemo");
-        bool success = false; //Variabila pt inchidere popup
-
+        
 
         [TestMethod]
         public void TestMethod_Sigemo()
         {
+            status_net();
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(SIGEMO_TARGET);
-            var wait = driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); // Waiting.... sa ma asigur ca incarca acel popup
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); // Waiting.... sa ma asigur ca incarca acel popup
             
 
-            while (true)
+            while (true) // inchidere popup
             {
                 try
                 {
                     var buton_close = driver.FindElement(By.XPath("/html/body/div[7]/div[1]/button")); // Acel popup futa-l draqu'
                     buton_close.Click();
-                    success = true;
-                }
-                catch (Exception ex)
-                {
-                    Assert.Fail(ex.Message);
-                    continue;
-                }
-                if (success)
                     break;
+                }
+                catch (Exception) { continue;}
              
             }
+            while (true) // inchidere mesaj cookie
+            {
+                try
+                {
+                    driver.FindElement(By.XPath("/html/body/div[2]/div/a[1]")).Click();
+                    break;
+                }
+                catch (Exception) { continue; }
+            }
             
+            // text_box searh
             IWebElement box_search = driver.FindElement(By.XPath("/html/body/div[7]/header/div[1]/div[3]/div[3]/div/div/div/span/input[2]"));
             box_search.Clear();
-            box_search.SendKeys(BRAND_NAME);
+            box_search.SendKeys(BRAND_NAME); // adaugare in text box variabila BRAND
+            status_net();
+            while (true)
+            {
+                try
+                {
+                    driver.FindElement(By.XPath("/html/body/div[7]/header/div[1]/div[3]/div[3]/div/div/div/button"))
+                    .Click(); // click icoana "lupa" (search)
+                    break;
+                }
+                catch (Exception) { continue; }
+            }
             
-            driver.FindElement(By.XPath("/html/body/div[7]/header/div[1]/div[3]/div[3]/div/div/div/button")).Click();
+            
+            driver.FindElement(By.XPath("/html/body/div[7]/div[2]/div/div/div/div/div/div[1]/div/div/div/div[5]/div/div/div/label/select")).Click(); // click box "latime anvelope"
+            driver.FindElement(By.XPath("/html/body/div[7]/div[2]/div/div/div/div/div/div[1]/div/div/div/div[5]/div/div/div/label/select/option[10]")).Click(); //selectare dimeniune latime 225
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("/html/body/div[7]/div[2]/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div/div/label/select/option[3]")).Click(); //selectare inaltime 45
 
             //var menuOpen = driver.FindElement(By.PartialLinkText("Logare"));
             //menuOpen.Click();
             //driver.Quit();
 
+        }
+
+        public void status_net()
+            // Verificare conexiune date
+        {
+            Ping pingul = new Ping();
+            while (true)
+                try
+                {
+                    
+                    var reply = pingul.Send("www.google.ro");
+                    if (reply.Status == IPStatus.Success)
+                        break;
+                }
+                catch (Exception) { continue; }
         }
     }
 }
